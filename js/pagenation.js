fca533132ec8bd7fb0261d78de487bb6ef7bc37e
Ryein Kim <ryein6896@gmail.com>
2023-08-20 16:20:26 +0900

    Add pagination on boards

--- js/pagenation.js
+++ js/pagenation.js
@@ -1,232 +1,73 @@
-/*
-const COUNT_PER_PAGE = 6; // 페이지 당 보여줄 게시물 수
-const numberButtonWrapper = document.querySelector('.number-button-wrapper'); // 페이지네이션 버튼 wrapper
-const ul = document.querySelector('ul'); // 게시물을 담을 unordered list
-const prevButton = document.querySelector('.prev-button'); // 이전 페이지 버튼
-const nextButton = document.querySelector('.next-button'); // 이후 페이지 버튼
-let pageNumberButtons; // 페이지 버튼들
+const postsPerPage = 5;
+const totalPosts = 15;
 
-let currentPage = 1; // 초기 페이지 번호
+const boardList = document.getElementById("board-list");
+const pagination = document.getElementById("pagination");
 
-/!**
- * 필요한 페이지 번호 개수 구하기
- * @returns {number} - 필요한 페이지 번호 개수
- *!/
-const getTotalPageCount = () => {
-    return Math.ceil(data.length / COUNT_PER_PAGE);
-};
+const posts = [
+    { title: "게시글 1 제목", author: "작성자 1", date: "작성일 1", content: "게시글 내용 1" },
+    { title: "게시글 2 제목", author: "작성자 2", date: "작성일 2", content: "게시글 내용 2" },
+    { title: "게시글 3 제목", author: "작성자 3", date: "작성일 3", content: "게시글 내용 3" },
+    { title: "게시글 4 제목", author: "작성자 4", date: "작성일 4", content: "게시글 내용 4" },
+    { title: "게시글 5 제목", author: "작성자 5", date: "작성일 5", content: "게시글 내용 5" },
+    { title: "게시글 6 제목", author: "작성자 6", date: "작성일 6", content: "게시글 내용 6" },
+    // ... 나머지 게시글 ...
+];
 
-/!**
- * 필요한 페이지 번호 수에 맞게 페이지 버튼 구성하기
- *!/
-const setPageButtons = () => {
-    numberButtonWrapper.innerHTML = '';
+function renderPosts(pageNumber) {
+    boardList.innerHTML = "";
 
-    for (let i = 1; i <= getTotalPageCount(); i++) {
-        numberButtonWrapper.innerHTML += `<span class="number-button"> ${i} </span`;
+    const startIndex = (pageNumber - 1) * postsPerPage;
+    const endIndex = Math.min(startIndex + postsPerPage, totalPosts);
+
+    for (let i = startIndex; i < endIndex; i++) {
+        const post = posts[i];
+
+        const article = document.createElement("article");
+        article.innerHTML = `
+                    <div class="post_head">${post.title}</div>
+                    <div class="post_body">${post.content}</div>
+                    <div class="post_info">${post.author} | ${post.date}</div>
+                `;
+
+        article.addEventListener("click", () => {
+            viewPost(i + 1); // 게시글 번호는 1부터 시작하므로 i + 1 사용
+        });
+
+        boardList.appendChild(article);
     }
+}
 
-    numberButtonWrapper.firstChild.classList.add('selected');
-    pageNumberButtons = document.querySelectorAll('.number-button');
-};
+function renderPagination(currentPage) {
+    pagination.innerHTML = "";
 
-/!**
- * 페이지에 해당하는 게시물 ul에 넣어주기
- * @param {number} pageNumber - 이동할 페이지 번호
- *!/
-const setPageOf = (pageNumber) => {
-    ul.innerHTML = '';
-
-    for (
-        let i = COUNT_PER_PAGE * (pageNumber - 1) + 1;
-        i <= COUNT_PER_PAGE * (pageNumber - 1) + 6 && i <= data.length;
-        i++
-    ) {
-        const li = document.createElement('li');
-
-        // 컨테이너
-        const postContainer = document.createElement('div');
-        postContainer.className = 'post-container';
-
-        // 글 번호
-        const postNumber = document.createElement('p');
-        postNumber.className = 'post-number';
-
-        // 글 제목
-        const postTitle = document.createElement('p');
-        postTitle.className = 'post-title';
-
-        postNumber.textContent = data[i - 1].postNumber;
-        postTitle.textContent = data[i - 1].title;
-
-        postContainer.append(postNumber, postTitle);
-        li.append(postContainer);
-        ul.append(li);
-    }
-};
-
-/!**
- * 페이지 이동에 따른 css 클래스 적용
- *!/
-const moveSelectedPageHighlight = () => {
-    const pageNumberButtons = document.querySelectorAll('.number-button'); // 페이지 버튼들
-
-    pageNumberButtons.forEach((numberButton) => {
-        if (numberButton.classList.contains('selected')) {
-            numberButton.classList.remove('selected');
+    const totalPages = Math.ceil(totalPosts / postsPerPage);
+    for (let i = 1; i <= totalPages; i++) {
+        const pageLink = document.createElement("a");
+        pageLink.href = "#";
+        pageLink.textContent = i;
+        if (i === currentPage) {
+            pageLink.classList.add("active");
         }
-    });
 
-    pageNumberButtons[currentPage - 1].classList.add('selected');
-};
+        pageLink.addEventListener("click", () => {
+            renderPosts(i);
+            renderPagination(i);
+        });
 
-setPageButtons();
-setPageOf(currentPage);
+        pagination.appendChild(pageLink);
 
-/!**
- * 페이지 번호 버튼 클릭 리스너
- *!/
-pageNumberButtons.forEach((numberButton) => {
-    numberButton.addEventListener('click', (e) => {
-        currentPage = +e.target.innerHTML;
-        console.log(currentPage);
-        setPageOf(currentPage);
-        moveSelectedPageHighlight();
-    });
-});
-
-/!**
- * 이전 버튼 클릭 리스너
- *!/
-prevButton.addEventListener('click', () => {
-    if (currentPage > 1) {
-        currentPage -= 1;
-        setPageOf(currentPage);
-        moveSelectedPageHighlight();
-    }
-});
-
-/!**
- * 이후 버튼 클릭 리스너
- *!/
-nextButton.addEventListener('click', () => {
-    if (currentPage < getTotalPageCount()) {
-        currentPage += 1;
-        setPageOf(currentPage);
-        moveSelectedPageHighlight();
-    }
-});*/
-const COUNT_PER_PAGE = 6; // 페이지 당 보여줄 게시물 수
-const numberButtonWrapper = document.querySelector('.number-button-wrapper'); // 페이지네이션 버튼 wrapper
-const ul = document.querySelector('ul'); // 게시물을 담을 unordered list
-const prevButton = document.querySelector('.prev-button'); // 이전 페이지 버튼
-const nextButton = document.querySelector('.next-button'); // 이후 페이지 버튼
-let pageNumberButtons; // 페이지 버튼들
-
-let currentPage = 1; // 초기 페이지 번호
-
-/**
- * 필요한 페이지 번호 개수 구하기
- * @returns {number} - 필요한 페이지 번호 개수
- */
-const getTotalPageCount = () => {
-    return Math.ceil(data.length / COUNT_PER_PAGE);
-};
-
-/**
- * 필요한 페이지 번호 수에 맞게 페이지 버튼 구성하기
- */
-const setPageButtons = () => {
-    numberButtonWrapper.innerHTML = '';
-
-    for (let i = 1; i <= getTotalPageCount(); i++) {
-        numberButtonWrapper.innerHTML += `<span class="number-button"> ${i} </span>`;
-    }
-
-    numberButtonWrapper.firstChild.classList.add('selected');
-    pageNumberButtons = document.querySelectorAll('.number-button');
-};
-
-/**
- * 페이지에 해당하는 게시물 ul에 넣어주기
- * @param {number} pageNumber - 이동할 페이지 번호
- */
-const setPageOf = (pageNumber) => {
-    ul.innerHTML = '';
-
-    for (
-        let i = COUNT_PER_PAGE * (pageNumber - 1);
-        i < COUNT_PER_PAGE * pageNumber && i < data.length;
-        i++
-    ) {
-        const li = document.createElement('li');
-
-        // 컨테이너
-        const postContainer = document.createElement('div');
-        postContainer.className = 'post-container';
-
-        // 글 번호
-        const postNumber = document.createElement('p');
-        postNumber.className = 'post-number';
-
-        // 글 제목
-        const postTitle = document.createElement('p');
-        postTitle.className = 'post-title';
-
-        postNumber.textContent = data[i].postNumber;
-        postTitle.textContent = data[i].title;
-
-        postContainer.append(postNumber, postTitle);
-        li.append(postContainer);
-        ul.append(li);
-    }
-};
-
-/**
- * 페이지 이동에 따른 css 클래스 적용
- */
-const moveSelectedPageHighlight = () => {
-    pageNumberButtons.forEach((numberButton) => {
-        if (numberButton.classList.contains('selected')) {
-            numberButton.classList.remove('selected');
+        if (i < totalPages) {
+            const separator = document.createElement("span");
+            separator.textContent = " | "; // 구분자 설정
+            pagination.appendChild(separator);
         }
-    });
-
-    pageNumberButtons[currentPage - 1].classList.add('selected');
-};
-
-setPageButtons();
-setPageOf(currentPage);
-
-/**
- * 페이지 번호 버튼 클릭 리스너
- */
-pageNumberButtons.forEach((numberButton) => {
-    numberButton.addEventListener('click', (e) => {
-        currentPage = +e.target.textContent;
-        setPageOf(currentPage);
-        moveSelectedPageHighlight();
-    });
-});
-
-/**
- * 이전 버튼 클릭 리스너
- */
-prevButton.addEventListener('click', () => {
-    if (currentPage > 1) {
-        currentPage -= 1;
-        setPageOf(currentPage);
-        moveSelectedPageHighlight();
     }
-});
+}
 
-/**
- * 이후 버튼 클릭 리스너
- */
-nextButton.addEventListener('click', () => {
-    if (currentPage < getTotalPageCount()) {
-        currentPage += 1;
-        setPageOf(currentPage);
-        moveSelectedPageHighlight();
-    }
-});
+function viewPost(postNumber) {
+    alert(`게시글 ${postNumber}를 클릭했습니다.`); // 실제 페이지로 이동하는 로직을 추가해야 합니다.
+}
+
+renderPosts(1);
+renderPagination(1);
